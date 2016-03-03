b7f7429b661fc63c12341b7995582e8f105c06e6
KatiaVille <evillev6@students.d125.org>
2016-03-03 14:09:38 -0600

    Cookie Class Added

---
+++ Why Not?/Cookies.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Cookies here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cookies
+{
+    // instance variables - replace the example below with your own
+    String flavor;
+
+    /**
+     * Constructor for objects of class Cookies
+     */
+    public Cookies(String flavor)
+    {
+        // initialise instance variables
+        this.flavor = flavor;
+    }
+
+   
+}
