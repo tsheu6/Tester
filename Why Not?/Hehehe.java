7068711c4fc5301b0cf62e11b5176cfb253c84c6
tsheu6 <tsheu6@students.d125.org>
2016-03-03 14:23:38 -0600

    File Added

---
+++ Why Not?/Hehehe.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Hehehe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hehehe
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args)
+    {
+        for (int i=0; i<10; i++)
+        {
+            System.out.print("he.");
+        }
+
+   
+    }
+}
